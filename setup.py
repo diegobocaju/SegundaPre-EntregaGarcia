4aaa62b705925f67c006a0404e16a140afe34086
Diego Garcia <diego12_23@yahoo.com.ar>
2023-06-09 20:31:34 -0300

    Archivos y base de datos generales

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Usuarios",
+    version="1.2",
+    description="Compra de televisores",
+    author="Diego Garcia",
+    author_email="diego12_23@yahoo.com.ar",
+    packages=["paquete"]
+)
